8872a9c63d5310c84e3ca19ab71492be60eb81a8
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2023-07-22 22:27:55 +0300

    Update python_advanced.py

--- python_advanced.py
+++ python_advanced.py
@@ -31,3 +31,24 @@ rub = (len(s) * 60) // 100
 cop = (len(s) * 60) % 100
 print(f'{rub} руб. {cop} коп.')
 
+# ЗАДАЧА ИОСИФА ФЛАВЕЛЯ. НУЖНО ЗАПОМНИТЬ, ЗАДАЧА РЕАЛЬНО КРУТАЯ!
+
+def opr(n, k):
+  count = 0
+  soldiers = [str(n - i) for i in range(n)]
+  
+  if k == 1:
+    return soldiers[0]
+  elif k != 1:
+    while (len(soldiers) != 1):
+      for i in range(len(soldiers)-1, -1, -1):
+        count += 1
+        if count == k:
+          soldiers.pop(i)
+          count = 0
+    return soldiers[0]
+
+n, k = int(input()), int(input())
+print(opr(n, k))
+
+#

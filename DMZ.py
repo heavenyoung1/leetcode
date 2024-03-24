f0106bb9903a887d5244e040e4fe4b8f591f37c8
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2024-03-24 12:30:53 +0500

    Add files via upload

--- DMZ.py
+++ DMZ.py
@@ -1,26 +1,27 @@
-import re
+import sys
+sys.path.append('/Users/Mefyod-EA/Documents/my_projects')
+from dmz_desc import dictionary
+with open('/Users/Mefyod-EA/Documents/my_projects/IN_DMZ.txt', 'r') as file:
+    with open('/Users/Mefyod-EA/Documents/my_projects/OUT_DMZ.txt', 'w') as output_file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            el = line.split('.')
+            module = el[2]
+            uso_kc = el[3].split('_')
+            if uso_kc[0] == 'USO':
+                uso_str = f'УСО {uso_kc[1]}, корзина {uso_kc[2]}, канал {uso_kc[3]}'
+            elif uso_kc[0] == 'KC':
+                uso_str = f'КЦ {uso_kc[1]}, канал {uso_kc[2]}'
+            else:
+                uso_str = 'RACKSTATE'
+            desc = el[4]
+            desc_key = dictionary.get(desc)
+            print(uso_kc)
+            output_file.write(f'Модуль {module}, {uso_str}, {desc_key}  \n')
+            print(line)
 
-# Открываем файл для чтения
-with open('IN_DMZ.txt', 'r') as file:
-    # Читаем все строки файла
-    lines = file.readlines()
 
-# Создаем словарь для хранения значений
-dictionary = {}
 
-# Проходимся по каждой строке
-for line in lines:
-    # Удаляем символ новой строки
-    line = line.strip()
 
-    # Пытаемся найти совпадение с регулярным выражением
-    match = re.match(r'diag\.ai\.uso3_3', line)
-    if match:
-        # Если совпадение найдено, присваиваем значение
-        dictionary[line] = 'Корзина АИ, модуль УСО 3.3'
-
-# Выводим результат
-with open('OUT_DMZ.txt', 'w') as output_file:
-    # Выводим результат в новый файл
-    for key, value in dictionary.items():
-        output_file.write(f'{key}: {value}\n')
+ #Root.Diag.DOs.USO_4_A2_05.ch_DI_28.ch_Kont

260a3926c22540a37bf87338baaa8c5b423b3047
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2024-06-05 17:12:21 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,8 @@
-from pydantic import BaseModel
-from typing import Union
+from pydantic import BaseModel, EmailStr
+from typing import Annotated
+from fastapi import FastAPI, Body, Path
 
-class User(BaseModel):
-    user: str = "Пользователь"
-    name: str
-    age: int
-    
-class Feedback(BaseModel):
-    name: str
-    message: str
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-class New_User(BaseModel):
-    id: int
-    name: str = "John Doe"
-    signup_ts: datetime | None = None
-    friends: list[int] = []
-
-external_data = {
-    "id": "123",
-    "signup_ts": "2017-06-01 12:22",
-    "friends": [1, "2", b"3"],
-}
+class CreateUser(BaseModel):
+    username: Annotated[str, Path(min_length=5, max_length=20)]
+    email: EmailStr 

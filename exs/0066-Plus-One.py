52a4e4b4ee04ace62d6f67e957b9aaa2ee98430e
Evgeny <jacksonou3@gmail.com>
2024-08-01 10:59:04 +0300

    :chart: Optimisation

--- exs/0066-Plus-One.py
+++ exs/0066-Plus-One.py
@@ -1,18 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i in reversed(range(len(digits))):
-            if digits[i] == 0:
-                digits[i] += 1
-                return digits
+            if digits[i] == 9:
+                digits[i] = 0
             else:
                 digits[i] += 1
-                if digits[i] == 10:
-                    digits[i] = 0
-                elif digits[i] != 0:
-                    return digits
-        if set(digits) == {0}:
-            digits.insert(0, 1)
-            return digits
-        else:
-            return digits
+                return digits
+        digits.insert(0, 1)
+        return digits
 

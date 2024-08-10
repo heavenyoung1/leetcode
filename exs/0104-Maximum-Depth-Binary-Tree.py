e93e11c6eddca23f8ab75299c8f88ccf12129726
Evgeny <jacksonou3@gmail.com>
2024-08-10 11:56:55 +0300

    :chart: Optimisation

--- exs/0104-Maximum-Depth-Binary-Tree.py
+++ exs/0104-Maximum-Depth-Binary-Tree.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -6,10 +7,6 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        left_depth, right_depth = 0, 0
         if not root:
             return 0
-        left_depth = self.maxDepth(root.left)
-        right_depth = self.maxDepth(root.right)
-
-        return max(left_depth, right_depth)
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))

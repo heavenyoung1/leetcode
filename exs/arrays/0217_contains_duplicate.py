9e5e3f8bb919c6c49b548d527ce9709a9a0aab8a
Eugene <jacksonou3@gmail.com>
2025-10-25 15:10:17 +0300

    ✅ Done

--- exs/arrays/0217_contains_duplicate.py
+++ exs/arrays/0217_contains_duplicate.py
@@ -9,3 +9,41 @@ class Solution:
 # Мое решение, 19мс, но там есть еще 1мс
 sol = Solution()
 print(sol.containsDuplicate([1,2,3,1]))
+
+# Вот это решение занимает 7мс
+class Solution1:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        original = set()
+        for i in nums:
+            if i in original:
+                return True
+            else:
+                original.add(i)
+        return False
+    
+
+sol1 = Solution1()
+print(sol1.containsDuplicate([1,2,3,1]))
+print(sol1.containsDuplicate([1,2,3,4]))
+
+# Решение при помощи отсортированного массива
+class Solution2:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+            
+# хех, это решение вообще 53мс, ну оно и понятно, тут сортировка массива
+sol2 = Solution2()
+print(sol2.containsDuplicate([1,2,3,1]))
+print(sol2.containsDuplicate([1,2,3,4]))
+
+class Solution3:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+    
+sol3 = Solution3()
+print(sol3.containsDuplicate([1,2,3,1]))
+print(sol3.containsDuplicate([1,2,3,4]))
